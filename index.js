7e10578bd329b25a4c56394a17310c8f999ce8c4
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-19 21:14:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,14 @@ function divide(a, b){
   return a / b
 }
 
-function inc(n)
+function inc(n){
+  return ++n
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
